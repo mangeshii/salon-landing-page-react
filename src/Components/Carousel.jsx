1dab6879de55b50c95abd563cdb0128b107de3d9
Mangeshi <thisismangeshi@gmail.com>
2021-11-23 22:48:18 +0530

    added navbar with animation

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -1,24 +1,11 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
-import { useState } from "react";
 import imgSlider1 from "../Images/img-slider1.jpg";
 import imgSlider2 from "../Images/img-slider2.jpg";
 import "./Carousel.css";
+import Navbar from "./Navbar";
 
 const Carousel = () => {
-    const [navBar, setNavBar] = useState(false);
-
-    const changeBackground = () => {
-        console.log(window.scroll)
-        if (window.scrollY >= 90) {
-            setNavBar(true);
-        } else {
-            setNavBar(false);
-        }
-    };
-
-    window.addEventListener("scroll", changeBackground);
-
     return (
         <>
             <div
@@ -26,20 +13,8 @@ const Carousel = () => {
                 className="carousel slide"
                 data-bs-ride="carousel"
             >
-                <div className={navBar ? 'nav-wrapper active' : 'nav-wrapper'}>
-                    <nav>
-                        <div className="logo">Betty</div>
-                        <div className="menu">
-                            <ul>
-                                <li>Home</li>
-                                <li>About Us</li>
-                                <li>Services</li>
-                                <li>Blog</li>
-                                <li>Contact</li>
-                            </ul>
-                        </div>
-                    </nav>
-                </div>
+                <Navbar />
+
                 <div className="carousel-inner">
                     <div className="carousel-item active">
                         <img
@@ -98,7 +73,40 @@ const Carousel = () => {
                 Deleniti vero fugit quisquam? Incidunt facilis fuga ex a veniam,
                 minus vero voluptatibus dolorum at obcaecati. Similique dolorem
                 corporis neque, aperiam, ea sunt eligendi fugit deleniti itaque
-                quidem doloremque obcaecati non laboriosam. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta nostrum facere deserunt optio eum, libero doloremque consectetur unde atque repellat qui. Laudantium, repellat necessitatibus. Laborum, explicabo modi? Rerum, soluta earum dignissimos veniam possimus voluptate aut culpa officia itaque perferendis exercitationem at dicta minima voluptates facere excepturi nemo tempora, quasi rem.
+                quidem doloremque obcaecati non laboriosam. Lorem ipsum dolor
+                sit, amet consectetur adipisicing elit. Soluta nostrum facere
+                deserunt optio eum, libero doloremque consectetur unde atque
+                repellat qui. Laudantium, repellat necessitatibus. Laborum,
+                explicabo modi? Rerum, soluta earum dignissimos veniam possimus
+                voluptate aut culpa officia itaque perferendis exercitationem at
+                dicta minima voluptates facere excepturi nemo tempora, quasi
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
+                dignissimos qui, repudiandae perferendis nemo minima nihil
+                quibusdam laboriosam nam inventore quam nisi odio, aut quia
+                eveniet velit! Doloremque rem quibusdam ipsam? Iste odio
+                recusandae dicta beatae pariatur optio delectus minima incidunt
+                perferendis doloremque blanditiis officiis alias laborum, enim
+                sint commodi numquam. Mollitia at a eius quas ut optio excepturi
+                ducimus accusamus officiis provident, omnis suscipit veritatis
+                perferendis perspiciatis odio id totam reiciendis voluptatibus,
+                in laudantium quibusdam qui cupiditate eveniet ipsum? Architecto
+                aperiam debitis corrupti necessitatibus quas animi, tempora
+                tenetur, quidem laboriosam laborum laudantium? Expedita
+                doloribus quidem dolores autem tempora consequatur perferendis
+                voluptatem incidunt excepturi vel nam, atque ipsum unde?
+                Quibusdam, voluptatum tempore totam dolores praesentium
+                consequatur pariatur tenetur repellendus in voluptatem tempora
+                possimus necessitatibus laudantium mollitia delectus cupiditate
+                velit beatae aperiam asperiores quam labore. Explicabo ex
+                possimus, necessitatibus, dolores esse exercitationem nisi
+                blanditiis autem sunt repellendus similique, tempora ipsam
+                aliquam aut vitae mollitia perspiciatis eligendi architecto
+                illo. Sint adipisci omnis similique ex quae, facilis ullam,
+                consequuntur maxime et ad tempore debitis dolore accusamus
+                dolorem suscipit maiores. Quia aspernatur perspiciatis corrupti
+                suscipit animi architecto pariatur illo eum minima quasi alias
+                possimus maxime, facilis neque quibusdam assumenda nihil sit.
+                Id, quos minima! rem.
             </h1>
         </>
     );
