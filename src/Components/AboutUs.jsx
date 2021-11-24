f300b179d604ef48ce3071d8656e4ff4e2f51184
Mangeshi <thisismangeshi@gmail.com>
2021-11-24 11:13:22 +0530

    added aboutus image

--- src/Components/AboutUs.jsx
+++ src/Components/AboutUs.jsx
@@ -1,4 +1,6 @@
 import "./AboutUs.css";
+import dottedImg from "../Images/dots.png";
+import aboutImg from "../Images/about.jpg";
 const AboutUs = () => {
     return (
         <>
@@ -8,7 +10,9 @@ const AboutUs = () => {
                         <div className="col-md-7 col-12">
                             <div className="p-3">
                                 <h1 className="about-heading">About Us</h1>
-                                <h5 className="sub-heading">Welcome bring the best service!</h5>
+                                <h5 className="sub-heading">
+                                    Welcome bring the best service!
+                                </h5>
                                 <div className="hori-line"></div>
                                 <h4 className="ad">
                                     Beauty bibendu tunc duntez variu estibulum
@@ -24,22 +28,41 @@ const AboutUs = () => {
                                     eu nunc bibe endum in finibus elit eget the
                                     solli citudin elit. Drae tnea rutierum in
                                     lacus ut euismod eleifend tore orci eu
-                                    ornare.</h6><h6 className="detail-para">Duis scelerisque the nibhse drana
-                                    moss pulvinar laie. Quality the biberon
-                                    endum in finbus elit egethe solli citudin
-                                    elit. Phasellus rutrum inle lacusut euismod.
+                                    ornare.
+                                </h6>
+                                <h6 className="detail-para">
+                                    Duis scelerisque the nibhse drana moss
+                                    pulvinar laie. Quality the biberon endum in
+                                    finbus elit egethe solli citudin elit.
+                                    Phasellus rutrum inle lacusut euismod.
                                     Vestibulum eleifend tortor eu misse endumin
-                                    finbus elit eget the soli citudin elit.</h6> <h6 className="detail-para">
+                                    finbus elit eget the soli citudin elit.
+                                </h6>{" "}
+                                <h6 className="detail-para">
                                     Vivense eu nunc bibe endum in finibus elit
                                     eget the solli citudin elit. Avensie
                                     rutrumin lacus ut euismod. Vestibulum
                                     eleifend tortor orci eu ornare sempe.
                                 </h6>
-                                <h4  className="ad">Betty White | Salon Owner</h4>
+                                <h4 className="ad">
+                                    Betty White | Salon Owner
+                                </h4>
                             </div>
                         </div>
-                        <div className="col-md-5 col-12">
-                            <div className="p-3">fnwfe;lfewvwevvvvvvv</div>
+                        <div className="col-md-5 col-12 img-wrapper">
+                            <div className="p-3">
+                                <div className="dotted-img-wrapper">
+                                    <img
+                                        className="dot-img "
+                                        src={dottedImg}
+                                        alt=""
+                                    />
+                                    <div className="img-border"></div>
+                                    <div className="about-img-wrapper">
+                                        <img className="about-img" src={aboutImg} alt="" />
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
